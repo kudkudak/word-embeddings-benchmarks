4894c32c2f9b277a5c245804be17117f5d5bd495
Stanislw Jastrzebski <staszek.jastrzebski@gmail.com>
2015-12-02 17:19:52 +0100

    Added WSR similarity dataset, #1

--- tests/test_fetchers.py
+++ tests/test_fetchers.py
@@ -6,7 +6,32 @@
 
 from web.datasets.analogy import fetch_google_analogy, fetch_msr_analogy, fetch_semeval_2012_2, \
     fetch_wordrep
-from web.datasets.similarity import fetch_simlex999
+from web.datasets.similarity import fetch_simlex999, fetch_WS353
+
+def test_similarity_fetchers():
+    data1 = fetch_WS353(which="set1")
+    data2 = fetch_WS353(which="set2")
+    data3 = fetch_WS353(which="similarity")
+    data4 = fetch_WS353(which="relatedness")
+    data5 = fetch_WS353(which="all")
+    V5 = set([" ".join(sorted(x)) for x in data5.X])
+    V1 = set([" ".join(sorted(x)) for x in data1.X])
+    V2 = set([" ".join(sorted(x)) for x in data2.X])
+    V3 = set([" ".join(sorted(x)) for x in data3.X])
+    V4 = set([" ".join(sorted(x)) for x in data4.X])
+
+    # sd and scores have same length
+    assert data1.sd.shape[0] == data1.y.shape[0]
+    assert data2.sd.shape[0] == data2.y.shape[0]
+
+    # WSR = WSR-SET1 u WSR-SET2
+    assert data5.X.shape[0] == 353
+    assert V5 == V2.union(V1)
+
+    assert V5 == V3.union(V4)
+
+    # Two word pairs reoccurr
+    assert len(V5) == 351
 
 def test_analogy_fetchers():
     data = fetch_msr_analogy()
--- web/datasets/similarity.py
+++ web/datasets/similarity.py
@@ -5,9 +5,85 @@
 """
 
 import pandas as pd
+import numpy as np
 
 from sklearn.datasets.base import Bunch
 from .utils import _get_dataset_dir, _fetch_files
+
+def fetch_WS353(which="all"):
+    """
+    Fetch WS353 dataset for testing attributional and
+    relatedness similarity
+
+    Parameters
+    -------
+    which : 'all': for both relatedness and attributional similarity,
+            'relatedness': for relatedness similarity
+            'similarity': for attributional similarity
+            'set1': as divided by authors
+            'set2': as divided by authors
+
+    Returns
+    -------
+    data : sklearn.datasets.base.Bunch
+        dictionary-like object. Keys of interest:
+        'X': matrix of 2 words per column,
+        'y': vector with scores,
+        'sd': vector of std of scores if available (for set1 and set2)
+
+    References
+    ----------
+    TODO: Add Indian Pines references
+
+    Notes
+    -----
+    TODO: Add notes
+
+    """
+    header = None
+    if which == "all":
+        header = 0
+        dataset_name = 'similarity/EN-WS353'
+        url = "https://www.dropbox.com/s/eqal5qj97ajaycz/EN-WS353.txt?dl=1"
+        file_name = "EN-WS353.txt"
+    elif which == "relatedness":
+        dataset_name = 'similarity/EN-WSR353'
+        url = "https://www.dropbox.com/s/x94ob9zg0kj67xg/EN-WSR353.txt?dl=1"
+        file_name = "EN-WSR353.txt"
+    elif which == "similarity":
+        dataset_name = 'similarity/EN-WSS353'
+        file_name = "EN-WSS353.txt"
+        url = "https://www.dropbox.com/s/ohbamierd2kt1kp/EN-WSS353.txt?dl=1"
+    elif which == "set1":
+        header = 0
+        dataset_name = 'similarity/EN-WS353-SET1'
+        file_name = "EN-WS353-SET1.txt"
+        url = "https://www.dropbox.com/s/opj6uxzh5ov8gha/EN-WS353-SET1.txt?dl=1"
+    elif which == "set2":
+        header = 0
+        dataset_name = 'similarity/EN-WS353-SET2'
+        file_name = "EN-WS353-SET2.txt"
+        url = "https://www.dropbox.com/s/w03734er70wyt5o/EN-WS353-SET2.txt?dl=1"
+    else:
+        raise RuntimeError("Not recognized which parameter")
+
+    data_dir = _get_dataset_dir(dataset_name, data_dir=None,
+                                verbose=0)
+    raw_data = _fetch_files(data_dir, [(file_name, url, {})],
+                            resume=True,
+                            verbose=0)[0]
+    data = pd.read_csv(raw_data, "\t", header=header)
+
+    # We basically select all the columns available
+    X = data.values[:, 0:2]
+    y = data.values[:, 2].astype(np.float)
+
+    # We have also scores
+    if data.values.shape[1] > 3:
+        sd = np.std(data.values[:, 2:15].astype(np.float), axis=1).flatten()
+        return Bunch(X=X, y=y, sd=sd)
+    else:
+        return Bunch(X=X, y=y)
 
 def fetch_simlex999():
     """
